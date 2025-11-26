828cbc0f94ff9f1ae641af277f1dd15ed8a93d28
Raghav0422 <kumarraghav561@gmail.com>
2025-11-27 01:17:53 +0530

    Initial commit

---
+++ tasks/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+# Create your views here.
+
+def index(request):
+    tasks= Task.objects.all()
+
+    context = {'tasks':tasks}
+    return render(request, 'list.html', context)
+    #return HttpResponse("Hello World")
